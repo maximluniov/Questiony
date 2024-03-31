78599b6d7ade234aa94389d182bff0939a51439b
maximluniov <mlunev330@gmail.com>
2024-03-31 21:58:20 +0200

    googleoauth and logout,cookies , mongodb(redacted)

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,8 @@
 //keys for production
 
 module.exports = {
-    googleClientID:process.env.GOOGLE_CLIEND_ID,
-    googleClientSecret:process.env.GOOGLE_CLIEND_SECRET,
+    googleClientID:process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret:process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY,
 }
