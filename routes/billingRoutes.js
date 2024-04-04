6f18b137c1c81ffaa56344d9a240892619cd0132
maximluniov <mlunev330@gmail.com>
2024-04-04 11:03:30 +0200

    fixing errors4

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -4,21 +4,16 @@ const stripe = require('stripe')(
 )
 const requireLogin = require('../middlewares/requireLogin')
 module.exports = app =>{
-    app.post(
-        '/api/stripe', requireLogin,
+    app.post('/api/stripe', requireLogin,
         async (req,res)=>{
-             await stripe.charges.create({
+            const charge = await stripe.charges.create({
                 amount: 500 ,
                 currency: 'usd',
                 description: '5$ for 5 credits',
                 source: req.body.id
             });
-            
-            req.user.credits += 5;
+           req.user.credits += 5;
            const user = await req.user.save();
-
            res.send(user);
         });
-
-
 };
